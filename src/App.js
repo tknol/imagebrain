df19d8de5e2703b1910874bbb5c7b7a559712f22
Thomas <tom.n.knol@gmail.com>
2018-08-05 15:40:43 -0500

    setting up the framework

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Particles from 'react-particles-js';
+
+const particlesParams = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+        params={particlesParams}
+      />
+
 		<Navigation />
-		{/*<Logo />
+		<Logo />
 		<ImageLinkForm />
 		<FaceRecognition />
-		*/}
       </div>
     );
   }
