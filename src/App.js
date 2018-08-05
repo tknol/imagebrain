cebb96672b3e633f63f39e72ff18d9d79b7a959f
Thomas <tom.n.knol@gmail.com>
2018-08-05 18:02:04 -0500

    add the box to surround a face when it is found

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
+import SignIn from './components/SignIn/SignIn';
 
 const app = new Clarifai.App({
- apiKey: 'APIKEY'
+ apiKey: '1cd868b2b9c04dcc8135921ba2dffd74'
 });
 
 const particlesParams = {
@@ -29,8 +30,28 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (response) => {
+      const clarifaiFace = response.outputs[0].data.regions[0].region_info.bounding_box;
+      const image = document.getElementById('inputImage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow : clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+  }
+
+  displayFaceBox(box) {
+    this.setState({box: box});
+    console.log(box);
   }
 
   onInputChange = (event) => {
@@ -39,14 +60,9 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
-        console.log(response);
-        // do something with response
-      },
-      function(err) {
-        // there was an error
-      }
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response))
+    .catch(err => console.log(err))
     );
   }
 
@@ -56,11 +72,11 @@ class App extends Component {
       <Particles className='particles'
         params={particlesParams}
       />
-
+      <SignIn />
 		<Navigation />
 		<Logo />
 		<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-		<FaceRecognition imageUrl={this.state.imageUrl} />
+		<FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
