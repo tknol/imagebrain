320896562a0027a55c30229d38483c357b9ee5a3
Thomas <tom.n.knol@gmail.com>
2018-08-05 16:09:30 -0500

    added the clarifai api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+ apiKey: 'YOURAPIKEY'
+});
 
 const particlesParams = {
   particles: {
@@ -19,6 +24,30 @@ const particlesParams = {
 };
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    app.models.predict(Clarifai.GENERAL_MODEL, "https://samples.clarifai.com/metro-north.jpg").then(
+      function(response) {
+        console.log(response);
+        // do something with response
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,7 +57,7 @@ class App extends Component {
 
 		<Navigation />
 		<Logo />
-		<ImageLinkForm />
+		<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
 		<FaceRecognition />
       </div>
     );
